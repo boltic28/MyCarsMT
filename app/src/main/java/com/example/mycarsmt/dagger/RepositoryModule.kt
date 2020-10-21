3724ad7c1e534e002f4ab99b853194ee13c9399f
nkttrvts <3119009@gmail.com>
2020-10-21 14:16:41 +0300

    2 v.3 added some modifications to RepositoryModule

--- app/src/main/java/com/example/mycarsmt/dagger/RepositoryModule.kt
+++ app/src/main/java/com/example/mycarsmt/dagger/RepositoryModule.kt
@@ -3,6 +3,7 @@ package com.example.mycarsmt.dagger
 import com.example.mycarsmt.datalayer.data.AppDatabase
 import com.example.mycarsmt.domain.service.car.CarRepository
 import com.example.mycarsmt.domain.service.car.CarRepositoryImpl
+import com.example.mycarsmt.domain.service.note.NoteRepository
 import com.example.mycarsmt.domain.service.note.NoteRepositoryImpl
 import com.example.mycarsmt.domain.service.part.PartServiceImpl
 import com.example.mycarsmt.domain.service.repair.RepairServiceImpl
@@ -23,7 +24,7 @@ class RepositoryModule(private val appDatabase: AppDatabase) {
 
     @Singleton
     @Provides
-    fun provideNoteService(): NoteRepositoryImpl = NoteRepositoryImpl(appDatabase.noteDao())
+    fun provideNoteRepository(): NoteRepository = NoteRepositoryImpl(appDatabase.noteDao())
 
     @Singleton
     @Provides
